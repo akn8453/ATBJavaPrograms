e97e2f5cec6ebd9744be2d4334db5f53369ba6c5
Akshay <akn8453@gmail.com>
2024-10-04 07:23:39 +0530

    My Initial commit regards to LabExercise & Task

---
+++ src/Oct/ex_02101024_MainMethod/Lab001.java
@@ -0,0 +1,7 @@
+package Oct.ex_02101024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Oct/ex_02101024_MainMethod/Task2_MultiplicationTable.java
@@ -0,0 +1,11 @@
+package Oct.ex_02101024_MainMethod;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int n=10, i;
+        for(i=1; i<=10; i++){
+            System.out.println(n + " x " + i + " = " + n * i );
+        }
+    }
+}
