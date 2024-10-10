e1f6601ca52086de299e4273b9043ffe74feaf31
Akshay <akn8453@gmail.com>
2024-10-10 18:50:47 +0530

    Task_10_10_2024_Example of litreals in java

---
+++ src/Task_10102024/Task1_Examples_of_Litreals.java
@@ -0,0 +1,48 @@
+package Task_10102024;
+
+public class Task1_Examples_of_Litreals {
+    public static void main(String[] args) {
+
+        // Integer literal
+        int numberOfEmployees = 25;
+
+        // Floating-point literal
+        double salary = 50000.75;
+
+        // Character literal
+        char grade = 'A';
+
+        // String literal
+        String companyName = "Xerus Systems";
+
+        // Boolean literal
+        boolean isOpenToday = true;
+
+        // Hexadecimal literal (integer)
+        int colorCode = 0xFF5733;
+
+        // Binary literal (integer)
+        int binaryNumber = 0b10101;
+
+        // Octal literal (integer)
+        int octalNumber = 0757;
+
+        // Null literal for object initialization
+        String department = null;
+
+        // Print literals
+        System.out.println("Number of Employees: " + numberOfEmployees);
+        System.out.println("Average Salary: ₹ " + salary + "");
+        System.out.println("Grade: " + grade);
+        System.out.println("Company Name: " + companyName);
+        System.out.println("Is the company open today? " + isOpenToday);
+        System.out.println("Color Code: " + colorCode);
+        System.out.println("Binary Number: " + binaryNumber);
+        System.out.println("Octal Number: " + octalNumber);
+        System.out.println("Department: " + department);
+    }
+}
+
+
+
+
