fd6cc52943aa701ad568a927b3945eac4bbe99b7
Akshay <akn8453@gmail.com>
2024-10-11 09:36:32 +0530

    Added files of 11th October

---
+++ src/Oct/ex_11102024_Operators/Lab040.java
@@ -0,0 +1,31 @@
+package Oct.ex_11102024_Operators;
+
+public class Lab040 {
+
+    public static void main(String[] args) {
+        // < (Less than)
+        //<= (Less than or equal to)
+        //> (Greater than)
+        //>= (Greater than or equal to)
+        //== (Equal to)
+        //!= (Not equal to)
+
+        //  //  > < , >= <= , != , ! -> true or false
+
+
+        int a = 10;
+        int b = 30;
+        boolean c = a > b;
+        System.out.println(c);
+
+        int age_ajay = 34;
+        int age_akshay = 34;
+        boolean result = age_akshay >= age_ajay;
+
+        // age_aksahy > age_ajay or age_akshay =age_ajay
+        System.out.println(result);
+
+
+
+    }
+}
