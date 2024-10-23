d965e3ba1c74bc9984fa59ff90e5e46bf029b271
Akshay <akn8453@gmail.com>
2024-10-23 19:33:47 +0530

    Added the files of 23rd Oct Class

---
+++ src/Oct/ex_23102024_Functions/Lab123.java
@@ -0,0 +1,23 @@
+package Oct.ex_23102024_Functions;
+
+public class Lab123 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int a) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main() {
+        return 18;
+    }
+
+    public static String main(String a, String b) {
+        return a+b;
+    }
+}
